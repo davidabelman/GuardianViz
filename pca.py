7b52780bea46a0b545a94103d4f2b677fcd5f019
David <davidabelman@gmail.com>
2014-07-11 14:52:30 +0100

    2d grid visualisation first version

--- pca.py
+++ pca.py
@@ -3,6 +3,9 @@ import options
 reload(options)
 from general_functions import *
 
+plot_pca = False
+plot_grid = False
+
 def unzip_dict_contents_modified(dictionary = None, combine_keys = ['tags']):
 	"""
 	Takes a dicitonary of things (each with an 'id' key in) and pulls out required fields in format:
@@ -15,8 +18,8 @@ def unzip_dict_contents_modified(dictionary = None, combine_keys = ['tags']):
 		assert 'id' in a, "-- Assert error: all entries in dictionary must have a key called 'id'"
 		total_string = ""
 		for key in combine_keys:
-			if type(a[key])==list:  # Join all elements in list separated by space (e.g. [list, of, tags] --> "list of tags")
-				add_string = " ".join(a[key])
+			if type(a[key])==list:  # Join all elements in list separated by space, replacing any spaces (e.g. ['my list', 'of', 'tags'] --> "mylist of tags")
+				add_string = " ".join([s.replace(" ","") for s in a[key]])
 				total_string += add_string
 			elif type(a[key])==unicode or type(a[key])==str:  # Just add the string as it is (e.g. body or headline)
 				total_string += a[key]+" "
@@ -34,60 +37,124 @@ def unzip_dict_contents_modified(dictionary = None, combine_keys = ['tags']):
 
 	return output
 
-def create_stopword_list(extra_words):
+def rank_cluster_posns(positions):
 	"""
-	Creates stopword list (adds extra words to original English set)
+	Take 2d array of positions, and return dictionary of cluster mappings so that the new '1' will be the left-most cluster etc.
 	"""
-	from sklearn.feature_extraction.text import TfidfVectorizer
-	original = list(TfidfVectorizer.get_stop_words(TfidfVectorizer(stop_words='english')))
-	return frozenset(original+extra_words)
+	return_dict = {}
+	x_dim = zip(*positions)[0]
+	t = zip(x_dim, range(len(x_dim)))
+	mapped = sorted(t, key=lambda x: x[0])
+	clusters = range(len(x_dim))
+	for i in range(len(mapped)):
+		return_dict[clusters[i]]=mapped[i][1]
+	return return_dict
 
+
+# Load articles and stopwords
 articles = load_pickle(options.current_articles_path)
 stopwords = create_stopword_list(extra_words = options.tfidf_extra_stopwords)
 
+# Implement TF-IDF to get an output matrix based on tags, headline, standfirst etc.
 from sklearn.feature_extraction.text import TfidfVectorizer
-article_tuples = unzip_dict_contents_modified(articles, ['tags'])  # [(article_id, "blah blah blah"), (article_id, "blah blah blah"), (article_id, "blah blah blah")]
+article_tuples = unzip_dict_contents_modified(articles, ['tags', 'headline'])  # [(article_id, "blah blah blah"), (article_id, "blah blah blah"), (article_id, "blah blah blah")]
 ids, tags, data = zip(*article_tuples)
 tfidf = TfidfVectorizer(stop_words=stopwords).fit_transform(data)
 
+# Apply PCA on the TF-IDF output
 from sklearn.decomposition import RandomizedPCA
 pca = RandomizedPCA(n_components=2)
 pca.fit(tfidf)
 output=pca.fit_transform(tfidf)
+o0 = range(len(output))  # Create indices of articles
+o1, o2 = zip(*output)[0], zip(*output)[1]  # Separate out the PCA components into lists
 
-# Plot the PCA output as it stands
+# Apply k-means clustering on the TFIDF output
+from sklearn.cluster import KMeans
+k_means = KMeans(init='k-means++', n_clusters=5, n_init=10)
+k_means.fit(tfidf)
+k_means_labels = k_means.labels_
+k_centres_2d = pca.fit_transform(k_means.cluster_centers_)
+cluster_map = rank_cluster_posns(k_centres_2d)
+
+# Optional : Plot the PCA output as it stands
 import pylab as pl
-o0 = range(len(output))  # Create indices
-o1, o2 = zip(*output)[0], zip(*output)[1]
-pl.scatter(o1,o2)
-print "This is the PCA space for our articles (unmodified)"
-pl.show()
+if plot_pca:
+	print "This is the PCA space for our articles (unmodified)"
+	pl.scatter(o1,o2)
+	pl.show()
 
-# Fix at 15*16 block
-to_fill = []   # we want [(0,4,2),(1,4,3),(2,9,1)] where we use (id, row, col)
+# Create an N*N block where N is ceil(sqrt) of total number of articles
 import pandas
 import numpy as np
+N = np.ceil(np.sqrt(len(output)))
 df = pandas.DataFrame([o1,o2]).transpose()
 df.columns = ['pca1','pca2']
+
+# Label PCA1 with label between 0 and N
 df = df.sort(column='pca1')
 df['pca1group']=o0
-df['pca1group'] = df['pca1group'].apply(lambda x: np.floor(x*1.0/16))
+df['pca1group'] = df['pca1group'].apply(lambda x: np.floor(x*1.0/N))
+
+# Label PCA2 with label between 0 and N (sorted within each PCA1 label)
 df = df.sort(columns=['pca1group', 'pca2'])
 df['pca2group']=o0
-df['pca2group'] = df['pca2group'].apply(lambda x: x%16)
-print df[0:35]
+df['pca2group'] = df['pca2group'].apply(lambda x: x%N)
 
+print "Here is the PCA dataframe with the grouped (%s x %s) data:" %(N, N)
+print df[0:35]
+print "etc..."
+
+# Extract data from dataframe to JSON for visualisation
 plot_list = []
 for i in range(len(df)):
 	index = df[i:i+1].index[0]
-	tag = tags[index]
+	id_ = ids[index]
+	cluster = k_means_labels[index]
 	pca1 = df['pca1group'][index]
 	pca2 = df['pca2group'][index]
-	plot_list.append((tag, pca1, pca2))
+	a = articles[id_]
+	plot_list.append({
+		'index' : str(index),
+		'id' : id_,
+		'x' : pca2,
+		'y' : pca1,
+		'tags' : a['tags'],
+		'fb' : a['facebook']['snapshot_of_total'] if a['facebook']['snapshot_of_total']!='Not extracted' else 5,
+		'url' : a['url'],
+		'img' : a['thumbnail'],
+		'headline' : a['headline'],
+		'standfirst' : a['standfirst'],
+		'cluster' : str(cluster_map[cluster]),
+		'r' : str(cluster+1), #k_centres_2d[cluster][0]+np.random.randint(-10,10)*1.0/90,
+		'g' : 100, #np.random.randint(140,150),
+		'b' : str(cluster) #k_centres_2d[cluster][1]+np.random.randint(-10,10)*1.0/90,
+		})
+
+import json
+j = json.dumps(plot_list)
+with open('html/grid.json', 'w') as the_file:
+	the_file.write(j)
+
+# Plot internally within Python
+if plot_grid:
+	plot_list = []
+	for i in range(len(df)):
+		index = df[i:i+1].index[0]
+		tag = tags[index]
+		cluster = k_means_labels[index]
+		pca1 = df['pca1group'][index]
+		pca2 = df['pca2group'][index]
+		plot_list.append((tag, pca1, pca2, cluster))
 
 
-pl.scatter(zip(*plot_list)[1], zip(*plot_list)[2])
-pl.rcParams.update({'font.size': 8})
-for i in range(len(o0)):
-	pl.annotate(zip(*plot_list)[0][i],xy=(zip(*plot_list)[1][i],zip(*plot_list)[2][i]))
-pl.show()
+	pl.scatter(zip(*plot_list)[1], zip(*plot_list)[2])
+	pl.rcParams.update({'font.size': 8})
+	for i in range(len(o0)):
+		r = 7
+		rand_a, rand_b = np.random.randint(-r,r)*1.0/20, np.random.randint(-r,r)*1.0/20
+		pl.annotate(str(zip(*plot_list)[3][i])+" "+zip(*plot_list)[0][i],xy=((zip(*plot_list)[1][i])+rand_a,(zip(*plot_list)[2][i])+rand_b))
+	pl.show()
+
+
+# Export all required data to JSON (facebook , cluster, URL , image, pagerank, etc.)
