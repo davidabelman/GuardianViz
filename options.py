dcc031a178759ee158a8eea02f920db71c2ac2a3
David <davidabelman@gmail.com>
2014-07-18 01:25:40 +0100

    Changed html folder structure and refreshed for latest G data

--- options.py
+++ options.py
@@ -12,8 +12,8 @@ path_choice = {
 	'sample' : "data/articles_subset.p",
 }
 current_articles_path = path_choice [ 'all' ]
-crawl_start_datetime = datetime.datetime(2014,7,11)
-crawl_end_datetime = datetime.datetime(2014,7,11)
+crawl_start_datetime = datetime.datetime(2014,7,13)
+crawl_end_datetime = datetime.datetime(2014,7,17)
 
 # Raw data from Guardian
 raw_pickle_path = "data/world_data.p"
