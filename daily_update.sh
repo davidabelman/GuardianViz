e7f311e7c643f2d724a5bbed51fc61d690f82d3a
David <davidabelman@gmail.com>
2014-07-18 01:45:18 +0100

    Fixed broken link and set to 7 day default for grid viz

--- daily_update.sh
+++ daily_update.sh
@@ -1,40 +1,40 @@
-# #!/bin/sh
+#!/bin/sh
 
-# clear
-# echo "\n======================================
-# RUNNING ALL DAILY UPDATES:
-# ====================================="
+clear
+echo "\n======================================
+RUNNING ALL DAILY UPDATES:
+====================================="
 
-# # Run crawl modules
-# echo "\n=======================
-# Crawling Guardian site:
-# ======================="
-# python guardian_main_crawl.py
+# Run crawl modules
+echo "\n=======================
+Crawling Guardian site:
+======================="
+python guardian_main_crawl.py
 
-# echo "\n=======================
-# Wrangling Guardian data:
-# ======================="
-# python guardian_main_wrangle.py
+echo "\n=======================
+Wrangling Guardian data:
+======================="
+python guardian_main_wrangle.py
 
-# echo "\n=======================
-# Crawling Facebook data:
-# ======================="
-# python facebook_crawl.py
+echo "\n=======================
+Crawling Facebook data:
+======================="
+python facebook_crawl.py
 
-# echo "\n=======================
-# Wrangling Facebook data:
-# ======================="
-# python facebook_wrangle.py
+echo "\n=======================
+Wrangling Facebook data:
+======================="
+python facebook_wrangle.py
 
-# echo "\n=======================
-# Crawling Guardian link data:
-# ======================="
-# python guardian_link_crawl.py
+echo "\n=======================
+Crawling Guardian link data:
+======================="
+python guardian_link_crawl.py
 
-# echo "\n=======================
-# Wrangling Guardian link data:
-# ======================="
-# python guardian_link_wrangle.py
+echo "\n=======================
+Wrangling Guardian link data:
+======================="
+python guardian_link_wrangle.py
 
 # Run algorithms
 # TF-IDF takes a long time...
